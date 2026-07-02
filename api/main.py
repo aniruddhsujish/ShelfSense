f8bb64d53d63b0a9935291147abb35a05fd909d7
Aniruddh Sujish <aniruddhsujish1113@gmail.com>
2026-07-02 14:12:17 +0530

    Added vercel deployment URL to CORS policy

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Book Recommender")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://shelf-sense-pied.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
